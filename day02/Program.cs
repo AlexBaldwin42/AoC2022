8834a711cd068abc109351bae7fef3bf34acad9c
Simian <alex@Mandelbrot>
2022-12-11 14:24:36 -0700

    day 2 part 2 completed

--- day02/Program.cs
+++ day02/Program.cs
@@ -2,108 +2,150 @@
 using System.Collections.Generic;
 var lines = File.ReadLines("input.txt");
 
-var translate = new Dictionary<char, char> ();
-translate.Add('A', 'X');
-translate.Add('B', 'Y');
-translate.Add('C', 'Z');
+calcPart2();
+void calcPart2(){
 
-var points = new Dictionary<char, int>();
-points.Add('X', 1);
-points.Add('Y', 2);
-points.Add('Z', 3);
+  // Part 2
 
-var words = new Dictionary<char, string> ();
-words.Add( 'X', "rock");
-words.Add('Y', "Paper");
-words.Add( 'Z', "Scissors");
+  var points = new Dictionary<char, int>();
+  points.Add('A', 1);
+  points.Add('B', 2);
+  points.Add('C', 3);
+  var words = new Dictionary<char, string> ();
+  words.Add( 'A', "Rock    ");
+  words.Add('B',  "Paper   ");
+  words.Add( 'C', "Scissors");
+  var winlosttieWords = new Dictionary<char, string> ();
+  winlosttieWords.Add( 'X', "Lose");
+  winlosttieWords.Add('Y', "Draw");
+  winlosttieWords.Add( 'Z', "Win");
+  //x lose
+  //y draw 
+  //z win 
+  // A Rock
+  // B Paper
+  // C Scissors
+  var winningHand = new Dictionary<char, char>();
+  winningHand.Add('A', 'B');
+  winningHand.Add('B', 'C');
+  winningHand.Add('C', 'A');
 
-
-int elfScore = 0;
-int myScore = 0; 
-foreach (var line  in lines)
-{
-  char elfHand = translate[line.Substring(0, 1)[0]]; 
-  char myHand = line.Substring(2, 1)[0];
-  Console.Write($"e:{words[elfHand]} M:{words[myHand]} ");
-
-  elfScore += points[elfHand];
-  myScore += points[myHand];
-//x rock
-//y paper
-//z sci 
-  if(elfHand != myHand)
+  // x lose y draw z wind
+  lines = File.ReadLines("input.txt");
+  int myScore = 0;
+  foreach (var line  in lines)
   {
-    if(elfHand == 'X' )
-    {
-      if(myHand == 'Y'){
-        myScore += 6;
-        Console.WriteLine("i win");
-      }else if(myHand == 'Z'){
-        Console.WriteLine("elf win");
-         elfScore +=6;
-      }
-    }
-    else if(elfHand == 'Y' )
-    {
-      if(myHand == 'Z'){
-        myScore += 6;
-        Console.WriteLine("i win");
-      }else if(myHand == 'X'){
-         elfScore +=6;
-        Console.WriteLine("elf win");
-      }
-    }
+    char elfHand =line.Substring(0, 1)[0]; 
+    //char myHand = line.Substring(2, 1)[0];
+    char winLostTie =line.Substring(2,1)[0];
+    char myHand = ' ';
 
-    else if(elfHand == 'Z' )
-    {
-      if(myHand == 'X'){
-        Console.WriteLine("i win");
-        myScore += 6;
-      }else if(myHand == 'Y'){
-         elfScore +=6;
-        Console.WriteLine("elf win");
-      }
-    }
+    switch(winLostTie){
+      case 'X':
+        // Lose
+        myHand =winningHand.Where(i=> i.Value == elfHand).Select(i=> i.Key).First();
+        break;
+      case 'Y':
+ 
+        // drawk
+        myHand = elfHand;
+        myScore += 3;
+        break;
+      case 'Z':
 
-  }else{
-    Console.WriteLine(" tie");
-    myScore+=3;
-    elfScore+=3;
+        // win
+        myHand = winningHand[elfHand];
+        myScore += 6;
+        break;
+    }
+    Console.WriteLine($"Elf: {words[elfHand]} Mine: {words[myHand]} {winlosttieWords[winLostTie]}");
+    myScore += points[myHand];
+    
+
+//    Console.Write($"e:{words[elfHand]} M:{words[myHand]} ");
+
+
+
   }
-
+  Console.WriteLine($"myScore {myScore}");
 }
 
-Console.WriteLine($"myscore {myScore} elfscore{elfScore}");
 
-// Part 2
 
-var points = new Dictionary<char, int>();
-points.Add('X', 1);
-points.Add('Y', 2);
-points.Add('Z', 3);
-//x rock
-//y paper
-//z sci
-var winningHand = new Dictionary<char, char>();
-translate.Add('X', 'Y');
-translate.Add('Y', 'Z');
-translate.Add('Z', 'X');
-// x lose y draw z wind
-lines = File.ReadLines("input.txt");
-myScore = 0;
-foreach (var line  in lines)
-{
-  char elfHand = translate[line.Substring(0, 1)[0]]; 
-  char myHand = line.Substring(2, 1)[0];
+//calcPart1();
+void calcPart1(){
 
-  Console.Write($"e:{words[elfHand]} M:{words[myHand]} ");
+  var translate = new Dictionary<char, char> ();
+  translate.Add('A', 'X');
+  translate.Add('B', 'Y');
+  translate.Add('C', 'Z');
 
-  if(myHand = 'X'){
-    // find losing hand 
-    winningHand.TryGetValue(elfHand).Key
+  var points = new Dictionary<char, int>();
+  points.Add('X', 1);
+  points.Add('Y', 2);
+  points.Add('Z', 3);
+
+  var words = new Dictionary<char, string> ();
+  words.Add( 'X', "rock");
+  words.Add('Y', "Paper");
+  words.Add( 'Z', "Scissors");
+
+
+  int elfScore = 0;
+  int myScore = 0; 
+  foreach (var line  in lines)
+  {
+    char elfHand = translate[line.Substring(0, 1)[0]]; 
+    char myHand = line.Substring(2, 1)[0];
+    Console.Write($"e:{words[elfHand]} M:{words[myHand]} ");
+
+    elfScore += points[elfHand];
+    myScore += points[myHand];
+    //x rock
+    //y paper
+    //z sci 
+    if(elfHand != myHand)
+    {
+      if(elfHand == 'X' )
+      {
+        if(myHand == 'Y'){
+          myScore += 6;
+          Console.WriteLine("i win");
+        }else if(myHand == 'Z'){
+          Console.WriteLine("elf win");
+          elfScore +=6;
+        }
+      }
+      else if(elfHand == 'Y' )
+      {
+        if(myHand == 'Z'){
+          myScore += 6;
+          Console.WriteLine("i win");
+        }else if(myHand == 'X'){
+          elfScore +=6;
+          Console.WriteLine("elf win");
+        }
+      }
+
+      else if(elfHand == 'Z' )
+      {
+        if(myHand == 'X'){
+          Console.WriteLine("i win");
+          myScore += 6;
+        }else if(myHand == 'Y'){
+          elfScore +=6;
+          Console.WriteLine("elf win");
+        }
+      }
+
+    }else{
+      Console.WriteLine(" tie");
+      myScore+=3;
+      elfScore+=3;
+    }
+
   }
 
-
+  Console.WriteLine($"myscore {myScore} elfscore{elfScore}");
 
 }
-
